832b7acc3ecef49edd958a5fb65eedcef14c1cb9
Joe Rybarczyk <joerybarczyk99@gmail.com>
2024-06-13 19:18:02 -0400

    refactor: remove li props from ToDoItem props

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -1,20 +1,13 @@
 import { Todo } from '../types';
 
-interface Props
-  extends React.DetailedHTMLProps<
-    React.LiHTMLAttributes<HTMLLIElement>,
-    HTMLLIElement
-  > {
+interface Props {
   todo: Todo;
   onTodoClick: () => void;
 }
 
-const ToDoItem: React.FC<Props> = ({ todo, onTodoClick, ...liProps }) => {
+const ToDoItem: React.FC<Props> = ({ todo, onTodoClick }) => {
   return (
-    <li
-      className="space-x-2 px-2 py-1 hover:cursor-pointer hover:bg-gray-200 hover:bg-opacity-10 rounded-md transition"
-      {...liProps}
-    >
+    <li className="space-x-2 px-2 py-1 hover:cursor-pointer hover:bg-gray-200 hover:bg-opacity-10 rounded-md transition">
       <label className="flex items-center hover:cursor-pointer">
         <input
           type="checkbox"
