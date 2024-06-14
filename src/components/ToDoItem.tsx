3f42b95d4e9911287b8e0a3a60c7d0cb0bd9269b
Joe Rybarczyk <joerybarczyk99@gmail.com>
2024-06-14 16:51:06 -0400

    Revert "test todo item swiping"
    This reverts commit 930cefe3d755a7ef3477e7855443214e64a502ad.

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Todo } from '../types';
 
 interface Props {
@@ -7,33 +6,8 @@ interface Props {
 }
 
 const ToDoItem: React.FC<Props> = ({ todo, onTodoClick }) => {
-  const [startX, setStartX] = useState<number>(0);
-  const [left, setLeft] = useState(0);
-
-  const handleTouchStart = (e: React.TouchEvent) => {
-    e.preventDefault();
-    setStartX(e.touches[0].clientX);
-  };
-
-  const handleTouchMove = (e: React.TouchEvent) => {
-    e.preventDefault();
-    const currentX = e.touches[0].clientX;
-    const diffX = currentX - startX;
-    setLeft(diffX);
-  };
-
-  const handleTouchEnd = () => {
-    setLeft(0);
-  };
-
   return (
-    <li
-      onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
-      onTouchEnd={handleTouchEnd}
-      style={{ transform: `translateX(${left}px)` }}
-      className="space-x-2 px-2 py-1 hover:cursor-pointer hover:bg-gray-200 hover:bg-opacity-10 rounded-md transition"
-    >
+    <li className="space-x-2 px-2 py-1 hover:cursor-pointer hover:bg-gray-200 hover:bg-opacity-10 rounded-md transition">
       <label className="flex items-center hover:cursor-pointer">
         <input
           type="checkbox"
