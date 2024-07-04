64f1a1b19f65c0b538b00d76278c9bb04e3c07c1
Joe Rybarczyk <joerybarczyk99@gmail.com>
2024-07-04 15:21:08 -0400

    style: update PrimaryButton styles

--- src/elements/PrimaryButton.tsx
+++ src/elements/PrimaryButton.tsx
@@ -5,7 +5,7 @@ const PrimaryButton: React.FC<
 > = ({ children, ...props }) => {
   return (
     <button
-      className="px-3 py-1 bg-indigo-500 active:bg-indigo-700 text-gray-100 dark:text-gray-800 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-600 focus:ring-offset-2 dark:focus:ring-offset-zinc-800 focus:ring-opacity-50 transition-colors"
+      className="px-3 py-1 bg-indigo-500 active:bg-indigo-700 hover:bg-indigo-700 text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-600 focus:ring-offset-2 dark:focus:ring-offset-zinc-800 focus:ring-opacity-50 transition-colors font-bold"
       {...props}
     >
       {children}
