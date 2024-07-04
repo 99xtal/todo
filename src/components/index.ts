8a696a327782d9373e674f101796be9c4f1a443e
Joe Rybarczyk <joerybarczyk99@gmail.com>
2024-07-04 15:53:59 -0400

    fix: remove undefined component import

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
-import SettingsForm from './SettingsForm';
 import ToDoForm from './ToDoForm';
 import ToDoItem from './ToDoItem';
 
-export { SettingsForm, ToDoForm, ToDoItem };
+export { ToDoForm, ToDoItem };
